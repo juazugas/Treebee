83cb869d9be9f906a40750be2778b81206a8e5b2
juazugas <juazuri@gmail.com>
2017-12-03 23:21:28 +0100

    Make work the prototype.

--- prototype/server.js
+++ prototype/server.js
@@ -8,6 +8,12 @@ const protodir = __dirname;
 app.use(express.static(protodir + '/public'));
 app.use('/lib', express.static(protodir + '/lib'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(SERVER_PORT, ()  => {
   console.log('server is up on port 3000');
 });
