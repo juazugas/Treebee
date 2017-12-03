9327820adee11f4daadfed11513524ffba0bee01
juazugas <juazuri@gmail.com>
2017-12-03 16:07:13 +0100

    Relocate project prototype.

--- prototype/server.js
+++ prototype/server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const SERVER_PORT = process.env.PORT || 3000;
 
 const app = express();
+const protodir = __dirname;
 
-app.use(express.static(__dirname + '/public'));
-app.use('/lib', express.static(__dirname + '/lib'));
+app.use(express.static(protodir + '/public'));
+app.use('/lib', express.static(protodir + '/lib'));
 
 app.listen(SERVER_PORT, ()  => {
   console.log('server is up on port 3000');
