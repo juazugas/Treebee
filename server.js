86b0fb02060ef8cbfd8cd19c0185736dd5083e78
juazugas <juazuri@gmail.com>
2017-12-03 16:00:12 +0100

    Serve libr/ace definition files.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const SERVER_PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/lib', express.static(__dirname + '/lib'));
 
 app.listen(SERVER_PORT, ()  => {
   console.log('server is up on port 3000');
